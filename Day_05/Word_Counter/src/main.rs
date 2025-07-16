705259ece19eacccc9750c6980bf25d752c79ca6
FaizanCyber007 <faizanjaved246@gmail.com>
2025-07-17 01:20:03 +0500

    Rust Projects

--- Day_05/Word_Counter/src/main.rs
+++ Day_05/Word_Counter/src/main.rs
@@ -1,3 +1,37 @@
+use std::env;
+use std::fs::File;
+use std::io::{Read};
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() != 2 {
+        println!("Usage: cargo run <filepath>");
+        return;
+    }
+
+    let file_path = &args[1];
+
+    let mut file: File = match File::open(&file_path) {
+        Ok(file) => file,
+        Err(_) => {
+            println!("Problem reading file. Try again!");
+            return;
+        }
+    };
+
+    let mut content = String::new();
+
+    if let Err(err) = file.read_to_string(&mut content) {
+        println!("Error opening file {}!", err);
+        return;
+    }
+
+    let word_count = count_words(&content);
+
+    println!("Total number of words in this file is {}", word_count);
+}
+
+fn count_words(a: &str) -> usize {
+    a.split_whitespace().count()
 }
